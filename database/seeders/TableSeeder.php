281e8e8852bf22a0797d6d1fe0f56a1a61088681
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-26 02:27:47 +0200

    exercise completed

--- database/seeders/TableSeeder.php
+++ database/seeders/TableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TableSeeder extends Seeder
 {
@@ -15,6 +16,19 @@ class TableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 12; $i++) {
+            $newTrain= new Train();
+            $newTrain->brand = $faker->word();
+            $newTrain->station = $faker->city();
+            $newTrain->destination = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->arrival_date_time = $faker->dateTime();
+            $newTrain->code = $faker->randomDigit();
+            $newTrain->carriege = $faker->randomDigit();
+            $newTrain->on_time =  $faker->boolean();
+            $newTrain->cancelled = $faker->boolean();
+            $newTrain->save();
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>ciao sono la home</div>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">ECCO I TUOI TRENI!</h1>
+            @foreach ($trains as $train)
+                <div class="col-3">
+                    <div class="card my-3" style="width: 18rem;">
+                        <div class="card-body">
+                          <h5 class="card-title">Azienda: {{$train->brand}}</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">Parte da: {{$train->station}}</h6>
+                          <div>Arriva a: {{$train->destination}}</div>
+                          <div>Partenza ore: {{$train->departure_time}}</div>
+                          <div>Arrivo alle: {{$train->arrival_date_time}}</div>
+                          <div>Codice: {{$train->code}}</div>
+                          <div>Numero carrozze: {{$train->carriege}}</div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
